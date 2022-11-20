c2b5ad02285fa1203487959430bfdf5aadaea951
mehedi-hasan <mdmehedihasan2810@gmail.com>
2022-11-20 21:58:55 +0600

    initial

---
+++ src/components/index.js
@@ -0,0 +1,29 @@
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import Button from "./Button";
+import Billing from "./Billing";
+import Business from "./Business";
+import CardDeal from "./CardDeal";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import FeedBackCard from "./FeedBackCard";
+import Footer from "./Footer";
+import GetStarted from "./GetStarted";
+import Stats from "./Stats";
+import Testimonials from "./Testimonials";
+
+export {
+    Navbar,
+    Hero,
+    Button,
+    Billing,
+    Business,
+    CardDeal,
+    Clients,
+    CTA,
+    FeedBackCard,
+    Footer,
+    GetStarted,
+    Stats,
+    Testimonials
+};
